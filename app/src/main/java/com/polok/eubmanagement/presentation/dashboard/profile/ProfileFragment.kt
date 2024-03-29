ff17c5f7b6e7d53b946acdf80a0776e3163c0776
daudhrimon <daudhrimon@gmail.com>
2024-03-29 15:15:25 +0600

    update ongoing

--- app/src/main/java/com/polok/eubmanagement/presentation/dashboard/profile/ProfileFragment.kt
+++ app/src/main/java/com/polok/eubmanagement/presentation/dashboard/profile/ProfileFragment.kt
@@ -1,10 +1,14 @@
 package com.polok.eubmanagement.presentation.dashboard.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.viewModels
+import com.google.firebase.auth.FirebaseAuth
 import com.polok.eubmanagement.base.BaseFragment
 import com.polok.eubmanagement.base.BaseViewModel
 import com.polok.eubmanagement.databinding.FragmentProfileBinding
+import com.polok.eubmanagement.presentation.auth.AuthActivity
+import com.polok.eubmanagement.presentation.classmatelist.ClassmateListActivity
 import com.polok.eubmanagement.presentation.dashboard.home.HomeViewModel
 import com.polok.eubmanagement.util.SharedPref
 import com.polok.eubmanagement.widget.PrimaryLoader
@@ -35,10 +39,27 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(
                 binding.blood.text = it.bloodGroup ?: ""
             }
         }
-        /*binding.signOutButton.setOnClickListener {
+        binding.signOutButton.setOnClickListener {
             viewModel.attemptSignOutUser(FirebaseAuth.getInstance())
-        }*/
+        }
+        binding.classMatesButton.setOnClickListener {
+            viewModel.fireNavigateEvent(1,null)
+        }
     }
 
     override fun initPrimaryLoader(): PrimaryLoader? = null
+
+    override fun onNavigateEvent(id: Int, bundle: Bundle?) {
+        super.onNavigateEvent(id, bundle)
+        when (id) {
+            0 -> {
+                startActivity(Intent(requireContext(), AuthActivity::class.java))
+                requireActivity().finish()
+            }
+
+            1 -> {
+                startActivity(Intent(requireContext(), ClassmateListActivity::class.java))
+            }
+        }
+    }
 }
